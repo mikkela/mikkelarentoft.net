b519489aab63fd94bb4ce4482d6688098aa2ff51
Mikkel Arentoft <mikkel@mikadocs.com>
2011-11-03 10:11:40 +0100

    Added title to static pages and refactored layout to use same code

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                      :content => " | Home")
     end
   end
 
@@ -13,6 +20,12 @@ describe PagesController do
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                      :content => " | Contact")
     end
   end
 
